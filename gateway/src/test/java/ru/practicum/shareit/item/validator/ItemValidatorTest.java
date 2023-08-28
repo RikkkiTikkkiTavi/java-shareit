1a8a0c56b256e20b89a09f096a8ad03e31f7dab9
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-08-28 19:15:55 +0300

    add new test

--- gateway/src/test/java/ru/practicum/shareit/item/validator/ItemValidatorTest.java
+++ gateway/src/test/java/ru/practicum/shareit/item/validator/ItemValidatorTest.java
@@ -2,8 +2,11 @@ package ru.practicum.shareit.item.validator;
 
 
 import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.exception.ItemValidationException;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -27,5 +30,11 @@ class ItemValidatorTest {
     void itemDescriptionMustBeNotNull() {
         itemDto = new ItemDto(1, "ItemOne", null, true, 0);
         assertThrows(ItemValidationException.class, () -> ItemValidator.checkItem(itemDto));
+    }
+
+    @Test
+    void commentTextMustBeNotEmpty() {
+        CommentDto commentDto = new CommentDto(1, "author", "", LocalDateTime.now());
+        assertThrows(ItemValidationException.class, () -> ItemValidator.checkComment(commentDto));
     }
 }
