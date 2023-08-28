570ccce39a80eb93b61415cc317f5ceb07d8976e
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-08-28 18:56:33 +0300

    delete unused classes

--- server/src/main/java/ru/practicum/shareit/booking/exception/BookingValidateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.booking.exception;
-
-public class BookingValidateException extends RuntimeException {
-    public BookingValidateException(String message) {
-        super(message);
-    }
-}
