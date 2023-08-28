cd671db7703e8fb283be1d36256866a5e6d418a1
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2023-08-28 19:03:23 +0300

    delete unused handlers

--- gateway/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -4,13 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.shareit.booking.exception.BookingNotFoundException;
 import ru.practicum.shareit.booking.exception.BookingValidateException;
 import ru.practicum.shareit.booking.exception.UnsupportedStateException;
-import ru.practicum.shareit.item.exception.ItemNotFoundException;
 import ru.practicum.shareit.item.exception.ItemValidationException;
-import ru.practicum.shareit.user.exception.DuplicateEmailException;
-import ru.practicum.shareit.user.exception.UserNotFoundException;
 import ru.practicum.shareit.user.exception.UserValidationException;
 
 @RestControllerAdvice
@@ -19,20 +15,6 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidation(final UserValidationException e) {
-        return new ErrorResponse(
-                e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleDuplication(final DuplicateEmailException e) {
-        return new ErrorResponse(
-                e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleValidation(final ItemNotFoundException e) {
         return new ErrorResponse(
                 e.getMessage());
     }
@@ -47,20 +29,6 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidation(final BookingValidateException e) {
-        return new ErrorResponse(
-                e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleValidation(final UserNotFoundException e) {
-        return new ErrorResponse(
-                e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleValidation(final BookingNotFoundException e) {
         return new ErrorResponse(
                 e.getMessage());
     }
